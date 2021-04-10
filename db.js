7184ef52b2926cf6ad60db255c4a114073519771
newbeeguy <mskjavascript@gmail.com>
2021-04-10 19:16:03 +0300

    fasd

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ module.exports = new Sequelize(
     process.env.DB_PASSWORD,
     {
         dialect: 'postgres',
-        host: process.env.DB_HOST,
+        host: process.env.DATABASE_URL ,
         port: process.env.DB_PORT
     }
 )
