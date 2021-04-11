777b246567cbfc6cbf56c081639f8c9b06e8ce9b
newbeeguy <mskjavascript@gmail.com>
2021-04-11 07:49:03 +0300

    asd

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const {Sequelize} = require('sequelize')
 
 console.log(process.env.DATABASE_URL)
-console.log(process.env.DATABASE_URL)
+
 module.exports = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
